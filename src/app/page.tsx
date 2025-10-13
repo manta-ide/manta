fde92233e1bd8166f12c0cc1ea0e48bd319c855a
makosst <191634637+makosst@users.noreply.github.com>
2025-10-12 22:27:38 -0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,6 @@ export default function Home() {
         {layersSidebarOpen && (
           <LayersSidebar
             open={layersSidebarOpen}
-            onClose={() => setLayersSidebarOpen(false)}
           />
         )}
       </div>
